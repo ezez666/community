bd8ac3756f44693305bfdebc4c17e724625d50d4
luoji <771558805@qq.com>
2020-03-07 23:57:01 +0800

    register

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,15 +1,46 @@
 package com.nowcoder.community.controller;
 
 
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/register",method = RequestMethod.GET)
     public String getRegisterPage(){
         return "/site/register";
+    }
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
+
+    @RequestMapping(path = "/register",method = RequestMethod.POST)
+    public String register(Model model, User user){
+        Map<String,Object> map = userService.register(user);
+
+        if(map==null||map.isEmpty()){
+            model.addAttribute("msg","注册成功，我们已向你发送邮件");
+            model.addAttribute("target","/index");
+            return "/mail/activation";
+        }else{
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+            return "/site/register";
+        }
+
     }
 }
